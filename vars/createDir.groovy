0c45d6df40119462be975d8f1057ab4645815564
Demaxd <1demaxd1@gmail.com>
2026-05-28 23:47:34 +0300

    замена cmd на powershell

--- vars/createDir.groovy
+++ vars/createDir.groovy
@@ -1,18 +1,6 @@
 def call(String path, boolean cleanDir = false) {
-    if (!path?.trim()) error("mkdir: path is empty")
-    
-    if (cleanDir && fileExists(path)) {
-        dir(path) { deleteDir() }
-    }
-    
-    if (isUnix()) {
-        sh "mkdir -p '${path}'"
-    } else {
-        // Таймаут 2 минуты — чтобы не держать очередь
-        timeout(time: 2, unit: 'MINUTES') {
-            echo "DEBUG: перед mkdir, path = '${path}'"
-            bat "@if not exist \"${path}\" mkdir \"${path}\""
-            echo "DEBUG: после mkdir"
-        }
-    }
+powershell """
+    Write-Host 'Creating directory: ${path}'
+    New-Item -ItemType Directory -Force -Path '${path}' -ErrorAction Stop
+"""
 }
